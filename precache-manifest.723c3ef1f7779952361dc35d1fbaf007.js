4c42ee5ce8ad1e154ce5b67f897cb37cefb03f13
Ayodeji Ajayi <ajayi.ayyodeji@gmail.com>
2023-08-21 03:32:45 +0100

    Updates

---
+++ precache-manifest.723c3ef1f7779952361dc35d1fbaf007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9cbd587e5b4d922baf4b7f085f57bf",
+    "url": "/TF-React-Video-Classifier/index.html"
+  },
+  {
+    "revision": "f9cbf9f3e8a2f0549dae",
+    "url": "/TF-React-Video-Classifier/static/css/main.ad48efa9.chunk.css"
+  },
+  {
+    "revision": "5a421579a98c77aa16c5",
+    "url": "/TF-React-Video-Classifier/static/js/2.144ee49b.chunk.js"
+  },
+  {
+    "revision": "1c8cc4c678a177ed6438",
+    "url": "/TF-React-Video-Classifier/static/js/3.ef47b266.chunk.js"
+  },
+  {
+    "revision": "f9cbf9f3e8a2f0549dae",
+    "url": "/TF-React-Video-Classifier/static/js/main.7366be8c.chunk.js"
+  },
+  {
+    "revision": "2f1b76c05127947a07fc",
+    "url": "/TF-React-Video-Classifier/static/js/runtime~main.933829d2.js"
+  }
+]);
